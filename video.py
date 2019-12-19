230e957dddc58a984b3e05abf699c162532ab210
Anton Bozhedarov <anton.bozhedarov@phystech.edu>
2019-12-19 16:23:59 +0300

    render video within pipeline

--- video.py
+++ video.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import requests
 import argparse
+from pipeline import *
+from skimage.transform import rescale 
 
 # url='http://10.16.114.79:8080/shot.jpg'
 
@@ -9,32 +11,64 @@ class VideoReadingError(Exception):
     pass
 
 def open_video(path):
- 
+    """
+    Opens video file at path
+    """
     # open video file
     cap = cv2.VideoCapture(path)
-    
-    while True:
+    j = 0
+    while(cap.isOpened()):
+        j+=1
         ret, img = cap.read()
+#         img = rescale(img, 0.4)
+        if j % 10 == 0:
+            img = np.asarray(img, dtype=np.uint8)
+            try:
+                img = pipeline(img)
+            except: 
+                pass
+            j = 0
+     
         cv2.imshow('frame', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 def open_web_cam():
-    
+    """
+    Opens video from internal web camera of laptop
+    """
     # open web cam
     cap = cv2.VideoCapture(0)
     
-    while True:
+    while(cap.isOpened()):
         ret, img = cap.read()
+                
+        try:
+            img = pipeline(img)
+        except: 
+            pass
+        
         cv2.imshow('frame', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 def open_web_ip(path):
+    """
+    Opens video from ip web camera
+    Arguments:
+        path (str) - link to the web camera
+             example: "http://192.168.0.103:8080/shot.jpg"
+    Returns: None
+    """
     while True:
-        img_res = requests.get(path)#"http://192.168.0.103:8080/shot.jpg")
+        img_res = requests.get(path)#
         img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
         img = cv2.imdecode(img_arr,-1)
+                
+        try:
+            img = pipeline(img)
+        except: 
+            pass
         cv2.imshow('frame', img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -53,7 +87,8 @@ if __name__ == "__main__":
         open_web_cam()
     elif mode == 'video':
         open_video(args.path)
-    elif mode == 'web_ip': 
+    elif mode == 'web_ip':
+        args.path ='http://192.168.1.64:8080/shot.jpg'
         open_web_ip(args.path)
     else:
         raise VideoReadingError("Wrong mode parameter")
