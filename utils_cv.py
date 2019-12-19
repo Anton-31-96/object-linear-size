8efca6b8a46abf3dda7c8690c4bf5edec520267a
fjosp <ppilyugina@gmail.com>
2019-12-19 18:53:29 +0300

    Update

--- utils_cv.py
+++ utils_cv.py
@@ -10,6 +10,33 @@ from skimage.morphology import dilation, disk
 import cv2
 import pickle
 
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+import cv2
+import collections
+import time
+from skimage import io
+from skimage.feature import canny
+from skimage.transform import rescale
+from skimage.morphology import dilation, disk
+import scipy 
+import skimage
+
+
+
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import imutils
+
+from tqdm import tqdm_notebook as tqdm
+
+import time
+
+from scipy.ndimage import distance_transform_edt
+
 def save_pkl(variable, name):
     name = name + '.pkl'
     output = open(name, 'wb')
@@ -51,59 +78,108 @@ def divide_into_boxes(image):
 def adjusting_brightness(image, a = 1.1, b = 0):
     return cv2.convertScaleAbs(image, alpha=a, beta=b)
 
-# def matrix(boxes, templates, correlation_threshold, alpha, beta):
-#     result = np.array([], dtype = 'int8')
-
-#     for box in boxes:
-#         box = adjusting_brightness(box, alpha, beta)
-#         number = 0
-#         correlations = []
-#         corr_res = [match_template(box, templates[i], pad_input = True) for i in range(18)]
-#         correlations = [i.max() for i in corr_res]
-#         if max(correlations) > correlation_threshold:
-#             number = int(correlations.index(max(correlations))) + 1
-#             if int(number) > 9:
-#                 number = int(number - 9)
-#         result = np.append(result, number)
-#     return result.reshape((9,9))
-
-# def recognize_digits(image, templates, correlation_threshold = 0.3, alpha = 1.1, beta = 0):
-#     boxes = divide_into_boxes(image)
-#     templates = [cv2.resize(i, (90, 90)) for i in templates]
-#     return matrix(boxes, templates, correlation_threshold, alpha, beta)
-
-
-# def plot_results(image, result, solution, alpha, beta):
-#     n, m = image.shape
-#     padding = n/9
-
-#     x_coordinates = np.array(range(0, 9))*(padding) + padding*0.4
-#     y_coordinates = np.array(range(0, 9))*(padding) + padding*0.7
-
-#     plt.figure(figsize = (5, 5))
-#     plt.imshow(adjusting_brightness(image, alpha, beta), cmap = 'gray')
-#     for i, x in enumerate(x_coordinates):
-#         for j, y in enumerate(y_coordinates):
-#             if result[j, i] == 0:
-#                 plt.text(x, y, solution[j, i], fontsize = 20, c = 'c')
-#             else:
-#                 plt.text(x + padding*0.2, y + padding*0.2, solution[j, i], fontsize = 10, c = 'r')
-
-#     plt.show()
+def normalize_image(image, image_colored, rescale_param = 0.5):
+    image_scaled = rescale(image, rescale_param)
+    edges = canny(image_scaled)
     
-# def whole_pipeline(image, templates, alpha = 1.1, beta = 0, correlation = 0.3, plot = True, printing = True):
-#     image = normalize_image(image)
-#     result = recognize_digits(image, templates, correlation, alpha, beta)
-#     if printing:
-#         print(result)
-#     try:
-#         solution = solve_sudoku(result)
-#         if printing:
-#             print('\n')
-#             print(solution)
-#         if plot:
-#             plot_results(image, result, solution, alpha, beta)
-#     except:
-#         print('Something went wrong :(, the original was:')
-#         show(image)
+    selem = disk(1)
+    edges = dilation(edges, selem)
+    
+    edges = (edges).astype(np.uint8)
+    img, ext_contours, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     show(img)
+    contour = max(ext_contours, key=cv2.contourArea)    
+    contour = contour.squeeze()
+    
+    epsilon = 0.05 * cv2.arcLength(contour, True)
+    corners = cv2.approxPolyDP(contour, epsilon, True).squeeze()
+    corners = (perspective.order_points(corners))
+    corners = corners/rescale_param
 
+#     plt.figure(figsize = (4, 4), dpi = 180)
+#     plt.imshow(image, cmap = 'gray')
+#     for i in range(len(corners)):
+#         plt.scatter(corners[i][0], corners[i][1], c = 'r', s = 0.4)
+#     plt.axis('off')
+    
+    size_square = min(image_scaled.shape)
+    tform = ProjectiveTransform()
+    tform.estimate(np.array([[0,0], [1020,0], [1020,720], [0, 720]]), corners)
+    image_warped = warp(image_colored, tform)[:720,:1020]
+
+    data = image_warped.astype('float64') / np.max(image_warped)
+    data = 255 * data
+    img = data.astype('uint8')
+    img = adjusting_brightness(img[30:-5, 15:-15], a = 1.7, b = 3)
+    return img, tform
+
+def pipeline(original):
+    def middle(x, y):
+        return ((x[0]+y[0])/2, (x[1]+y[1])/2)
+    try:
+        image_redone, tform = normalize_image(cv2.cvtColor(original, cv2.COLOR_RGB2GRAY),
+                                   original,
+                                   rescale_param = 0.6)
+    except:
+        return None
+    result = image_redone.copy()
+    
+    median_filtered = scipy.ndimage.median_filter(cv2.cvtColor(image_redone, cv2.COLOR_RGB2GRAY ), size=3)
+
+    threshold = skimage.filters.threshold_otsu(median_filtered)
+    predicted = np.uint8(median_filtered > 220) * 255
+    gray = cv2.GaussianBlur(median_filtered, (7, 7), 0)
+
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=2)
+    edged = cv2.erode(edged, None, iterations=1)
+
+    img, ext_contours, hierarchy = cv2.findContours(edged.copy(),
+                                                    cv2.RETR_EXTERNAL,
+                                                    cv2.CHAIN_APPROX_SIMPLE)
+
+    (cnts, _) = contours.sort_contours(ext_contours)
+    cnts = [cnt[:, 0] for cnt in cnts]
+    pixelsPerMetric = None
+
+    
+    for c in cnts:
+        if cv2.contourArea(c) < 200:
+            continue
+
+        box = cv2.boxPoints(cv2.minAreaRect(c)).astype('int32')
+        box = perspective.order_points(box).astype('int32')
+        cv2.drawContours(result, [box], -1, (255, 0, 116), 2)
+
+        top_left, top_right, bottom_right, bottom_left = box
+
+        top_x, top_y = middle(top_left, top_right)
+        bottom_x, bottom_y = middle(bottom_left, bottom_right)
+        left_x, left_y = middle(top_left, bottom_left)
+        right_x, right_y = middle(top_right, bottom_right)
+
+        distance_1 = dist.euclidean((top_x, top_y), (bottom_x, bottom_y))
+        distance_2 = dist.euclidean((left_x, left_y), (right_x, right_y))
+
+        dimA = distance_2 * 0.076
+        dimB = distance_1 * 0.076
+
+
+        cv2.circle(result, (int(top_x), int(top_y)), 5, (255, 0, 0), -1)
+        cv2.putText(result, "{:.1f} cm".format(dimA),
+            (int(top_x - 10), int(top_y - 10)), 0, 0.65, (255, 0, 0), 2)
+
+        cv2.circle(result, (int(right_x), int(right_y)), 5, (255, 0, 0), -1)
+        cv2.putText(result, "{:.1f} cm".format(dimB),
+            (int(right_x + 5), int(right_y)), 0, 0.65, (255, 0, 0), 2)
+        
+    result = np.pad(result, ((30,5), (15, 15), (0,0)), mode='constant') #pading is the same KOSTYL as before
+    result = warp(result, tform.inverse, output_shape=original.shape)
+    result = result.astype('uint8')
+    mask = np.nonzero(result[:,:,0])
+
+    # render borders on original image 
+    a = original.copy()
+    a[mask] = [200, 0, 0]
+    
+    return a
